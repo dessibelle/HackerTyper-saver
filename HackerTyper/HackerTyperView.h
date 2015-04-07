1972745a7c3989f4cd302c73aa59b14b3b16ee27
Simon Fransson <simon@dessibelle.se>
2015-04-07 21:44:18 +0200

    Initial Commit

---
+++ HackerTyper/HackerTyperView.h
@@ -0,0 +1,13 @@
+//
+//  HackerTyperView.h
+//  HackerTyper
+//
+//  Created by Simon Fransson on 2015-04-07.
+//  Copyright (c) 2015 Simon Fransson. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface HackerTyperView : ScreenSaverView
+
+@end
---
+++ HackerTyper/HackerTyperView.m
@@ -0,0 +1,52 @@
+//
+//  HackerTyperView.m
+//  HackerTyper
+//
+//  Created by Simon Fransson on 2015-04-07.
+//  Copyright (c) 2015 Simon Fransson. All rights reserved.
+//
+
+#import "HackerTyperView.h"
+
+@implementation HackerTyperView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
